5adacdc75acb263ba0dbdca169ccc1bcc3f1cbfe
QSouhardyo <quaziaumio98@gmail.com>
2022-10-12 22:16:24 +0600

    hook

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -27,7 +27,7 @@ const Blog = () => {
                 </Accordion.Panel>
                 <Accordion.Panel>
                     <Accordion.Title>
-
+                        What is useRef hooks in react?
                     </Accordion.Title>
                     <Accordion.Content>
                         <p className="mb-2 text-gray-500 dark:text-gray-400">
