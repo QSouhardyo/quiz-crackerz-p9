fb708af01a6fa76be69235789b4fe67083b8aa6b
QSouhardyo <quaziaumio98@gmail.com>
2022-10-12 22:03:43 +0600

    blog

---
+++ src/Components/Blog/Blog.js
@@ -0,0 +1,44 @@
+import { Accordion } from 'flowbite-react';
+import React from 'react';
+
+const Blog = () => {
+    return (
+        <div className='md:px-4 px-2 md:mt-5 mt-5'>
+            <Accordion>
+                <Accordion.Panel>
+                    <Accordion.Title>
+
+                    </Accordion.Title>
+                    <Accordion.Content>
+                        <p className="mb-2 text-gray-500 dark:text-gray-400">
+
+                        </p>
+                    </Accordion.Content>
+                </Accordion.Panel>
+                <Accordion.Panel>
+                    <Accordion.Title>
+
+                    </Accordion.Title>
+                    <Accordion.Content>
+                        <p className="mb-2 text-gray-500 dark:text-gray-400">
+
+                        </p>
+                    </Accordion.Content>
+                </Accordion.Panel>
+                <Accordion.Panel>
+                    <Accordion.Title>
+
+                    </Accordion.Title>
+                    <Accordion.Content>
+                        <p className="mb-2 text-gray-500 dark:text-gray-400">
+
+                        </p>
+                    </Accordion.Content>
+                </Accordion.Panel>
+            </Accordion>
+
+        </div>
+    );
+};
+
+export default Blog;
